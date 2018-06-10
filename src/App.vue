3a1267d689d0378e0bd5739fbf3b952a1da5b0d3
Femonzor <wayneyang3@gmail.com>
2018-06-10 23:47:54 +0800

    header init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <main-header></main-header>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import MainHeader from '@/components/main-header/index.vue';
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+@Component({
+  components: {
+  MainHeader
+  }
+  })
+export default class App extends Vue {
+}
+</script>
+
+<style lang="stylus" scoped>
 </style>
